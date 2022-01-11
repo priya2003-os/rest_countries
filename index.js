63a44399ea8cd5fc6d8c3efe4f3cdff6cb31cfec
Administrateur <priyaalq@gmail.com>
2022-01-11 17:16:29 +0100

    First version

---
+++ index.js
@@ -0,0 +1,18 @@
+//recuperer la liste des noms des pays avec une fonction via URL
+async function fetchCountries() {
+
+    const response = await fetch('https://restcountries.com/v3.1/region/europe/?fields=name');
+    const countries = await response.json();
+
+
+    let listName = "";
+    countries.forEach(country => {
+        listName += `<li>${country.name.official}</li>`;
+
+    });
+    const list = document.querySelector('#country');
+    list.innerHTML = listName;
+}
+window.addEventListener('load', fetchCountries());
+
+
