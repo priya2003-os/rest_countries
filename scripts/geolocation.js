41c07c22429ee00e98dae87e6c9a24cf6bb3aca9
Administrateur <priyaalq@gmail.com>
2022-01-29 14:39:40 +0100

    correction selected

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -9,31 +9,36 @@ async function render() {
     countries.forEach(country => {
 
 
-        content += `<option id="option" value="${country.cca2}">${country.name.official}</option>`;
+        // content += `<option id="option" value="${country.cca2}">${country.name.official}</option>`;
 
-        map += `src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyC-psA1diyTHzfueedcPfB1-EAVzBhobGg
-              &q=Space + Needle, Seattle + WA"`;
+
+
+        if (country.cca2 == "FR") {
+            content += `<option value="${country.cca2}" selected>${country.name.official}</option>`;
+        } else {
+            content += `<option value="${country.cca2}">${country.name.official}</option>`;
+        }
+
+        console.log(content);
+
+        const selectCountry = document.getElementById('countrySelect');
+        selectCountry.addEventListener('change', (event) => {
+            const result = document.querySelector('#map');
+            result.innerHTML = `src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyC-psA1diyTHzfueedcPfB1-EAVzBhobGg
+                    &q=${event.target.value}"`;
+
+        });
+
 
     });
 
-    // const selected = document.getElementById('select');
-    // console.log(selected);
-    // let x = content;
-    // // console.log(x);
-    // function y() {
 
-    // }
-    // if (value = "FR") {
-    //     x += `<option value="FR" selected>French Republic</option>`;
 
-    //     console.log(x);
-    // };
 
     const select = document.querySelector('#countrySelect');
     const googleMap = document.querySelector('#frame');
     googleMap.innerHTML = map;
     select.innerHTML = content;
-    select.value = "FR";
 
     // faire une fonction onchange avec eventlistener (ex kokou : let mapsOfCountries = `https://www.google.com/maps/embed/v1/view?key=AIzaSyAWHSmK2mrqS2Dg_E8DPygukjvKPCOqcKk&center=${event.target.value.capitalInfo}&zoom=10`)
 
@@ -46,10 +51,6 @@ async function render() {
 
 
 
-    // async function rend() {
-    //     const response = await fetch(`'https://restcountries.com/v3.1/capital/${country.capital}'`);
-    //     const countri = await response.json();
-    //     console.log(countri);
     // }
 
 }
@@ -57,4 +58,3 @@ async function render() {
 
 window.addEventListener('load', render());
 
-// value = "FR" selected
